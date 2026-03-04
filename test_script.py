36d5aa8a8f3d2c2253e6da2038a9417f2c9620ac
Arvindvincentz <155831830+Arunaah@users.noreply.github.com>
2026-03-05 01:47:53 +0530

    ae: create test_script.py

---
+++ test_script.py
@@ -0,0 +1,11 @@
+import unittest
+import json
+import script
+
+class TestParseJson(unittest.TestCase):
+    def test_parse_json(self):
+        json_input = '{"test_script.py": "import unittest\nimport json\nimport script\n\nclass TestParseJson(unittest.TestCase):
+    def test_parse_json(self):
+        json_input = '{"test_script.py": "import unittest\nimport json\nimport script\n\nclass TestParseJson(unittest.TestCase):
+    def test_parse_json(self):
+        json_input = '{"'
