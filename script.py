e8c545adbec0a4038b77c6d3783d51898ee52298
Arvindvincentz <155831830+Arunaah@users.noreply.github.com>
2026-03-05 01:47:52 +0530

    ae: create script.py

---
+++ script.py
@@ -0,0 +1,11 @@
+import json
+
+def parse_json(json_str):
+    return json.loads(json_str)
+
+if __name__ == "__main__":
+    json_input = '{"test_script.py": "import unittest\nimport json\nimport script\n\nclass TestParseJson(unittest.TestCase):
+    def test_parse_json(self):
+        json_input = '{"test_script.py": "import unittest\nimport json\nimport script\n\nclass TestParseJson(unittest.TestCase):
+    def test_parse_json(self):
+        json_input = '{"
